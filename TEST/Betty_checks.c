8b45d62a19e09063b98a0fbe26919e3ea2d28e1c
dsimwinga <infor.dsimwinga@gmail.com>
2023-10-20 21:45:03 +0200

    simple_shell_group_project

--- TEST/Betty_checks.c
+++ TEST/Betty_checks.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * main - Entry point of the program
+ * betty_checks - Entry point of the program
  *
  * Return: Always 0 (Success)
  */
 int betty_checks(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		printf("Hello, Betty!\n");
 	}
 
-    return (0);
+	return (0);
 }
 
--- TEST/handle.c
+++ TEST/handle.c
@@ -1,13 +1,19 @@
 #include "main.h"
 
+/**
+ *parse_input - my main function
+ *@input: inputs from user
+ *@args: arguments
+ */
 void parse_input(char *input, char **args)
 {
 	int i = 0;
 	char *token = strtok(input, " ");
+
 	while (token != NULL)
 	{
 		args[i++] = token;
 		token = strtok(NULL, " ");
 	}
-    args[i] = NULL; /* Set the last element to NULL to indicate the end of arguments*/
+	args[i] = NULL;
 }
